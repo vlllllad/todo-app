13ecdc1a1c869fd39c623ea9fec78a5ea3d2eccc
vlllllad <shevchuk.vs2004@gmail.com>
2019-05-23 18:43:20 +0300

    Version 1.0

---
+++ src/components/item-status-filter/item-status-filter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './item-status-filter.css';  
+
+const filterButtons = [
+  { name: 'all', label: 'All' },
+  { name: 'active', label: 'Active' },
+  { name: 'done', label: 'Done' }
+];
+
+const ItemStatusFilter = ({filter, onFilterChange = () => {}}) => {
+
+  const buttons = filterButtons.map(({name, label}) => {
+    const isActive = name === filter;
+    const classNames = 'btn ' + (isActive ? 'btn-success' : 'btn-outline-secondary');
+
+    return (
+      <button key={name}
+              type="button"
+              onClick={() => onFilterChange(name)}
+              className={classNames}>{label}</button>
+    );
+  });
+
+  return (
+    <div className="btn-group">
+      { buttons }
+    </div>
+  );
+};
+
+export default ItemStatusFilter;
